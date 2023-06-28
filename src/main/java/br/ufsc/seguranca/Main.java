f9df097667913c89eefeaf77504b4f7da437d879
LucasTruppel <lucasctruppelm@gmail.com>
2023-06-27 23:31:21 -0300

    Teste da biblioteca liboqs-java

--- src/main/java/br/ufsc/seguranca/Main.java
+++ src/main/java/br/ufsc/seguranca/Main.java
@@ -1,7 +1,50 @@
 package br.ufsc.seguranca;
 
+import org.openquantumsafe.Common;
+import org.openquantumsafe.Signature;
+import org.openquantumsafe.Sigs;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Supported signatures:");
+        Common.print_list(Sigs.get_supported_sigs());
+        System.out.println();
+
+        System.out.println("Enabled signatures:");
+        Common.print_list(Sigs.get_enabled_sigs());
+        System.out.println();
+
+        byte[] message = "Trabalho de segurança".getBytes();
+
+        String sig_name = "Dilithium5";
+        Signature signer = new Signature(sig_name);
+        signer.print_details();
+        System.out.println();
+
+        long t = System.currentTimeMillis();
+        byte[] signer_public_key = signer.generate_keypair();
+        long timeElapsed = System.currentTimeMillis() - t;
+
+        System.out.println("Signer public key:");
+        System.out.println(Common.chop_hex(signer_public_key));
+        System.out.println("\nIt took " + timeElapsed + " millisecs to generate the key pair.");
+
+        t = System.currentTimeMillis();
+        byte[] signature = signer.sign(message);
+        System.out.println("It took " + (System.currentTimeMillis() - t) + " millisecs to sign the message.");
+
+        Signature verifier = new Signature(sig_name);
+
+        t = System.currentTimeMillis();
+        boolean is_valid = verifier.verify(message, signature, signer_public_key);
+        System.out.println("It took " + (System.currentTimeMillis() - t) + " millisecs to verify the signature.");
+
+        System.out.println("\nSignature:");
+        System.out.println(Common.chop_hex(signature));
+
+        System.out.println("\nValid signature? " + is_valid);
+
+        signer.dispose_sig();
+        verifier.dispose_sig();
     }
 }
